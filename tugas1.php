b755d2cee6c20d2edf8f1612a62d8dce55348d8d
Adi <adiandroo@yahoo.com>
2020-09-28 20:45:43 +0700

    Tugas 1 PHP
    Tugas 1 PHP saya kumpulkan ke Niomic

---
+++ tugas1.php
@@ -0,0 +1,23 @@
+<?php 
+
+$harga_1 = 13000;
+$harga_2 = 15000;
+$harga_3 = 1000;
+$harga_4 = 5000;
+$harga_5 = 3000;
+$harga_6 = 4000;
+
+echo "<h1>Daftar Menu</h1>";
+echo "";
+print "<pre>
+<li>Ayam Goreng     = Rp. $harga_1</li>
+<li>Ayam Bakar      = Rp. $harga_2</li>
+<li>Ayam Sayur      = Rp. $harga_1</li>
+<li>Tempe Goreng    = Rp. $harga_3</li>
+<li>Tahu Goreng     = Rp. $harga_3</li>
+<li>Soto            = Rp. $harga_5</li>
+<li>Nasi Putih      = Rp. $harga_4</li>
+<li>Es Teh          = Rp. $harga_6</li>
+<li>Es Tebu         = Rp. $harga_4</li>
+</pre>"
+?>
